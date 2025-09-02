1f2dae66a31d72ab87b11c0529c7fd61652650bd
KySmAsTeR64 <obitznils@gmail.com>
2025-09-02 14:38:59 +0200

    W.I.P. doesnt work yet!!!

---
+++ src/functions/camera.h
@@ -0,0 +1,16 @@
+# ifndef CAMERA_H
+# define CAMERA_H
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+typedef struct{
+    GLFWwindow * window;
+    int xWindowSize;
+    int yWindowSize;
+} WindowData;
+
+extern WindowData windowData;
+
+//extern void loadOBJ(const char *filename, GLfloat *out, int *werte);
+# endif
